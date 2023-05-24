397a9ef61cfad4cc7118aa8e15e8deb09a80c9e2
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2023-05-24 18:49:51 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,13 @@
+import throttle from "lodash.throttle";
 
+function saveForm() {
+    const emailInput = document.querySelector('input[name="email"]');
+    const textInput = document.querySelector('textarea[name="message"]');
+
+    const form = {
+        email: emailInput.value,
+        message: textInput.value
+    }
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(form));
+}
