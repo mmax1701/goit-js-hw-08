f5ee8467b5fa7ed23dcc8c01d46774e6beaee1e6
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2023-05-24 17:55:56 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player');
+
+const currentTime = function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+    
+player.on('timeupdate', throttle(currentTime, 1000));
+
+const saveTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(saveTime);
