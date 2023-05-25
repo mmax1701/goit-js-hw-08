c2a58683dc52e9b4b737ad0e63dfabe934599d7c
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2023-05-25 12:50:32 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,15 +3,47 @@ import throttle from "lodash.throttle";
 const form = document.querySelector('.feedback-form');
 const emailInput = document.querySelector('input[name="email"]');
 const messageInput = document.querySelector('textarea[name="message"]');
+const button = document.querySelector('button');
+
+let formValues = {
+  email: emailInput.value,
+  message: messageInput.value
+};
 
 function saveForm() {
-    const formValues = {
-        email: emailInput.value,
-        message: messageInput.value
-    };
+    formValues.email = emailInput.value;
+    formValues.message = messageInput.value;
 
     localStorage.setItem('feedback-form-state', JSON.stringify(formValues));
 }
 
-const saveEmail = emailInput.addEventListener('input', saveForm);
-const saveMessage = messageInput.addEventListener('input', saveForm);
+const throttleSaveForm = throttle(saveForm, 500);
+
+emailInput.addEventListener('input', throttleSaveForm);
+messageInput.addEventListener('input', throttleSaveForm);
+
+
+
+function getLocal() {
+    const getValue = localStorage.getItem('feedback-form-state');
+    if (getValue) {
+        const parseValue = JSON.parse(getValue);
+        emailInput.value = parseValue.email;
+        messageInput.value = parseValue.message;
+        formValues = parseValue;
+    }
+}
+
+getLocal();
+
+function buttonClick() {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        localStorage.removeItem('feedback-form-state');
+        emailInput.value = '';
+        messageInput.value = '';
+        console.log(formValues);
+})
+}
+
+buttonClick();
